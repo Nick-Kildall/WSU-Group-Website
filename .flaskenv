6d383830c5d9630c41d28171cdbf82f8c49df253
Nick Kildall <nkildall@gmail.com>
2021-10-20 13:57:24 -0700

    Version v.00 - created skeleton code.

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=r_connect.py
+FLASK_DEBUG=1
---
+++ app/Controller/routes.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import sys
+from flask import Blueprint
+from flask import render_template, flash, redirect, url_for, request
+from config import Config
+
+from app import db
+
+bp_routes = Blueprint('routes', __name__)
+bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
+
+
+@bp_routes.route('/', methods=['GET'])
+@bp_routes.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
