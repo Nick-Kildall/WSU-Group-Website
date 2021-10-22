5c0315bf1d1fcbf764bfd5d4510d8fd628074758
Nick Kildall <nkildall@gmail.com>
2021-10-21 20:21:02 -0700

    Nick - Added a route for the student edit form

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -3,7 +3,7 @@ import sys
 from flask import Blueprint
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import  current_user, login_required
-from app.Controller.forms import FacultyEditForm
+from app.Controller.forms import FacultyEditForm, StudentEditForm
 from config import Config
 from app.Model.models import Faculty
 
@@ -31,3 +31,22 @@ def f_edit_profile():
     #         flash("Your changes have been saved")
     #         return render_template(url_for('routes.index'))
     return render_template('f_edit_profile.html', title='Edit Profile', form=eform)
+
+@bp_routes.route('/f_edit_profile', methods=['GET','POST'])
+def s_edit_profile():
+    sform = StudentEditForm()
+    # if request.method=='POST':
+    #     if sform.validate_on_submit():
+    #         current_user.phone_num = sform.phone_num.data
+    #         current_user.set_password(sform.password.data) 
+    #         current_user.major = sform.major.data
+    #         current_user.gpa = sform.gpa.data
+    #         current_user.grad_date = sform.grad_date.data
+    #         current_user.tech_electives = sform.tech_electives.data
+    #         current_user.languages = sform.languages.data
+    #         current_user.prior_exp = sform.prior_exp.data
+    #         db.session.add(current_user)
+    #         db.session.commit()
+    #         flash("Your changes have been saved")
+    #         return render_template(url_for('routes.index'))
+    return render_template('s_edit_profile.html', title='Edit Profile', form=sform)
