5ddf358b955f9b0d1e57ceced221ca29fdac8adc
Sejal Welankar <sejal.welankar@wsu.edu>
2021-11-04 13:28:51 -0700

    Testing the inhertiance model

--- app/Model/models.py
+++ app/Model/models.py
@@ -3,12 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from flask_login import UserMixin
 from app import login
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 @login.user_loader  
 def load_user(id):
     return User.query.get(int(id))
-
 
 userInterests = db.Table('userInterests',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
@@ -63,7 +64,7 @@ class Interest(db.Model):
     def __repr__(self):
         return '<ID: {} Name: {}>'.format(self.id,self.name)
 
-
+"""
 class User(UserMixin, db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(120),unique=True,index=True)
@@ -156,3 +157,73 @@ class Faculty(User, db.Model):
     pass
 
 '''
+"""
+
+class User(Base):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    username=db.Column(db.String(120),unique=True,index=True)
+    #email = db.Column(db.String(120),unique=True,index=True)
+    password_hash=db.Column(db.String(128))
+    firstname = db.Column(db.String(64))
+    lastname = db.Column(db.String(64))
+    phone_num=db.Column(db.String(15))
+    wsu_id=db.Column(db.String(10),index=True)
+    # userInterests
+    interests = db.relationship("Interest",
+        secondary = userInterests,
+        primaryjoin=(userInterests.c.user_id == id),
+        backref=db.backref('userInterests',
+        lazy='dynamic'),
+        lazy='dynamic')
+
+    __mapper_args__ = {
+        "polymorphic_identity": "user",
+        'with_polymorphic': '*',
+        "polymorphic_on": type
+    }
+
+    
+    def __repr__(self):
+        return '<ID: {} Username: {}>'.format(self.id,self.username)
+    
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+    
+    def get_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    #type = Column(String(50))
+
+   
+    def __repr__(self):
+        return "User %s" % self.name
+
+
+class Faculty(User):
+    __tablename__ = "faculty"
+    id = db.Column(db.ForeignKey("user.id"), primary_key=True)
+    
+    __mapper_args__ = {"polymorphic_identity": "faculty"}
+
+
+
+
+class Student(User):
+    __tablename__ = "user"
+    id = db.Column(db.ForeignKey("person.id"), primary_key=True)
+    status = db.Column(db.String(30))
+    major = db.Column(db.String(64), default = "")
+    gpa = db.Column(db.String(5),default = "")
+    grad_date = db.Column(db.String(64),default = "")
+    tech_electives = db.Column(db.String(1000),default = "")
+    languages = db.Column(db.String(1000),default = "")
+    prior_exp = db.Column(db.String(10000),default = "")
+    
+
+    __mapper_args__ = {"polymorphic_identity": "manager"}
+
+
+
+
