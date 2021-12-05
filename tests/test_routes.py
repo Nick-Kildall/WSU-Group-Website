d3400426382990a0ee080e27f919ba9f553912d0
Sejal Welankar <sejal.welankar@wsu.edu>
2021-12-05 14:36:55 -0800

    Testing functions

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,7 +8,7 @@ Resources:
 import os
 import pytest
 from app import create_app, db
-from app.Model.models import User, Post, Tag
+from app.Model.models import User, Student, Faculty, Post, Interest, Apply, Application
 from config import Config
 
 
@@ -40,35 +40,53 @@ def test_client():
  
     ctx.pop()
 
-def new_user(uname, uemail,passwd):
-    user = User(username=uname, email=uemail)
+def new_student(uname,uphn, ufirstname,ulastname,uwsuid,umajor,ugpa, ugraddate,utechelectives,ulanguages, upriorexp, passwd):
+    user = Student(username = uname, 
+            phone_num = uphn, firstname = ufirstname,
+            lastname = ulastname, wsu_id = uwsuid,
+            major = umajor, gpa = ugpa,
+            grad_date = ugraddate, tech_electives = utechelectives,
+            languages = ulanguages, prior_exp = upriorexp, user_type="Student")
     user.set_password(passwd)
     return user
 
-
-def init_tags():
-    # initialize the tags
-    if Tag.query.count() == 0:
-        tags = ['funny','inspiring', 'true-story', 'heartwarming', 'friendship']
-        for t in tags:
-            db.session.add(Tag(name=t))
+def init_interests():
+    # initialize the interests
+    if Interest.query.count() == 0:
+        interests = ['Artificial Intelligence/Machine Learning','Front End Developement', 'Back End Developement', 'Data Science', 'Software Engineering',
+            'Web Development', 'Full Stack', 'Mobile Application', 'Game Development', 'Cybersecurity', 'Financial Analysis', 'Blockchain']
+        for i in interests:
+            db.session.add(Interest(name=i))
         db.session.commit()
-        print(tags)
     return None
 
 @pytest.fixture
 def init_database():
     # Create the database and the database table
     db.create_all()
-    # initialize the tags
-    init_tags()
+    init_interests()
+    
     #add a user    
-    user1 = new_user(uname='sakire', uemail='sakire@wsu.edu',passwd='1234')
+    studentUser = new_student(uname = "selina@wsu.edu",uphn = "1234", ufirstname = "Selina",ulastname = "Nguyen",uwsuid = "1234567",umajor = "Computer Science",ugpa= "4.0", ugraddate = "May 2023",utechelectives = "CS 121",ulanguages = "C, C++, Python", upriorexp = " TA for 121", passwd = "123")
+    # studentUser.interests.append("Data Science")
+    # studentUser.interests.append("Full Stack")
+    
     # Insert user data
-    db.session.add(user1)
+    db.session.add(studentUser)
     # Commit the changes for the users
     db.session.commit()
 
     yield  # this is where the testing happens!
 
     db.drop_all()
+
+def test_register_page(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/register' page is requested (GET)
+    THEN check that the response is valid
+    """
+    # Create a test client using the Flask application configured for testing
+    response = test_client.get('/student_registration')
+    assert response.status_code == 200
+    assert b"Student Registration" in response.data
