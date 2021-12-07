284dfb39bb07677914c6cdd0701276ef8772e66b
Sejal Welankar <sejal.welankar@wsu.edu>
2021-12-07 12:26:51 -0800

    Added multiple tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -61,6 +61,7 @@ def new_faculty(uname,uphn, ufirstname,ulastname,uwsuid, passwd):
            user_type="Faculty")
     user.set_password(passwd)
     return user
+    
 
 def init_interests():
     # initialize the interests
@@ -79,9 +80,11 @@ def init_database():
     db.create_all()
     init_interests()
     #add a user    
-    studentUser = new_student(uname = 'selina@wsu.edu',uphn = '1234', ufirstname = 'Selina',ulastname = 'Nguyen',uwsuid = '1234567',umajor = 'Computer Science',ugpa= '4.0', ugraddate = 'May 2023',utechelectives = 'CS 121',ulanguages = 'C, C++, Python', upriorexp = 'TA for 121', passwd = '123')
+    studentUser = new_student(uname = 'selina@wsu.edu',uphn = '1234', ufirstname = 'Selina',ulastname = 'Nguyen',uwsuid = '1234567',
+        umajor = 'Computer Science',ugpa= '4.0', ugraddate = 'May 2023',utechelectives = 'CS 121',
+        ulanguages = 'C, C++, Python', upriorexp = 'TA for 121', passwd = '123')
     facultyUser = new_faculty(uname = "sakire@wsu.edu",uphn = "1234", ufirstname = "Sakire",ulastname = "Arslan Ay",uwsuid = "7891012", passwd="123")
-    
+
     # Insert user data
     db.session.add(facultyUser)
     db.session.add(studentUser)
@@ -114,7 +117,6 @@ def test_faculty_register_page(test_client):
     #https://gehrcke.de/2015/05/in-memory-sqlite-database-and-flask-a-threading-trap/
     # Create a test client using the Flask application configured for testing
     response = test_client.get('/faculty_registration')
-    print(response.data)
     assert response.status_code == 200
     assert b"Submit" in response.data
 
@@ -124,47 +126,49 @@ def test_faculty_register(test_client,init_database):
     WHEN the '/register' form is submitted (POST)
     THEN check that the response is valid and the database is updated correctly
     """
+    #uname = "sakire@wsu.edu",uphn = "1234", ufirstname = "Sakire",ulastname = "Arslan Ay",uwsuid = "7891012", passwd="123"
     # Create a test client using the Flask application configured for testing
     response = test_client.post('/faculty_registration', 
-                          data=dict(username = "selina@wsu.edu", firstname = "Selina", lastname = "Nguyen", phone_num = "3602928820", wsu_id = "11659253", user_type = "Faculty" ),
+                          data=dict(username = "sakire@wsu.edu", firstname = "Sakire", lastname = "Arslan Ay", phone_num = "1234", wsu_id = "7891012", user_type = "Faculty" ),
                           follow_redirects = True)
     assert response.status_code == 200
 
-    s = db.session.query(Faculty).filter(Faculty.username=='selina@wsu.edu')
-    assert s.first().username == 'selina@wsu.edu'
+    s = db.session.query(Faculty).filter(Faculty.username=='sakire@wsu.edu')
+    assert s.first().username == 'sakire@wsu.edu'
     assert s.count() == 1
-    print(response.data)
     assert b"Submit" in response.data  
 
-def test_student_register(test_client,init_database):
+def test_invalid_credentials_login(test_client,init_database):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/register' form is submitted (POST)
-    THEN check that the response is valid and the database is updated correctly
+    WHEN the '/login' form is submitted (POST) with wrong credentials
+    THEN check that the response is valid and login is refused 
     """
-    # Create a test client using the Flask application configured for testing
-    response = test_client.post('/student_registration', 
-                            data=dict(username = 'selina@wsu.edu',phone_num = '1234', 
-                            firstname = 'Selina',lastname = 'Nguyen',
-                            wsu_id = '1234567',major = 'Computer Science',
-                            gpa= '4.0', grad_date = 'May 2023',
-                            tech_electives = 'CS 121',languages = 'C, C++, Python', 
-                            prior_exp = 'TA for 121',user_type= 'Student', password = '123', password2 = '123'),
-                            follow_redirects = True)
-
+    response = test_client.post('/login', 
+                        data=dict(username='sejal123@wsu.edu', password='123',remember_me=False),
+                        follow_redirects = True)
     assert response.status_code == 200
+    assert b"Invalid username or password" in response.data
 
-    s = db.session.query(Student).filter(Student.username =='selina@wsu.edu')
-    print(s)
-    assert s.first().wsu_id == '1234567'
-    assert s.count() == 1
-    assert b"Submit" in response.data   
-    assert b"Please log in to access this page." in response.data
+def test_faculty_login_logout(request,test_client,init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' form is submitted (POST) with correct credentials
+    THEN check that the response is valid and login is succesfull 
+    """
+    response = test_client.post('/login', 
+                          data=dict(username='sakire@wsu.edu', password='123',remember_me=False),
+                          follow_redirects = True)
+    assert response.status_code == 200
+    assert b"Welcome to Research Connect!" in response.data
+
+    response = test_client.get('/logout',                       
+                          follow_redirects = True)
+    assert response.status_code == 200
+    assert b"Sign In" in response.data
 
 
-
-
-def test_login_logout(request,test_client,init_database):
+def test_student_login_logout(request,test_client,init_database):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/login' form is submitted (POST) with correct credentials
@@ -178,61 +182,44 @@ def test_login_logout(request,test_client,init_database):
 
     response = test_client.get('/logout',                       
                           follow_redirects = True)
-    print(response.data)
     assert response.status_code == 200
     assert b"Sign In" in response.data
 
 
-
-#def test_createpost(test_client,init_database):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/login' form is submitted (POST) with wrong credentials
-    THEN check that the response is valid and login is refused 
-    """
-#    response = test_client.post('/login', 
-#                          data=dict(username='selina@wsu.edy', password='123',remember_me=False),
-#                          follow_redirects = True)
-#    assert response.status_code == 200
-#    assert b"Invalid username or password" in response.data
-
-
-#def test_post(test_client,init_database):
-    """
-    GIVEN a Flask application configured for testing , after user logs in,
-    WHEN the '/postsmile' page is requested (GET)  AND /PostForm' form is submitted (POST)
-    THEN check that response is valid and the class is successfully created in the database
-    """
-
-"""
- #login
-    response = test_client.post('/login', 
-                        data=dict(username='sakire@wsu.edu', password='123',remember_me=False),
-                        follow_redirects = True)
-    assert response.status_code == 200
-    assert b"Welcome to Research Connect!" in response.data
+# def test_post(test_client,init_database):
+#     """
+#     GIVEN a Flask application configured for testing , after user logs in,
+#     WHEN the '/postsmile' page is requested (GET)  AND /PostForm' form is submitted (POST)
+#     THEN check that response is valid and the class is successfully created in the database
+#     """
+#     #login
+#     response = test_client.post('/login', 
+#                         data=dict(username='sakire@wsu.edu', password='123',remember_me=False),
+#                         follow_redirects = True)
+#     assert response.status_code == 200
+#     assert b"Welcome to Research Connect!" in response.data
     
-    #test the "PostSmile" form 
-    response = test_client.get('/createpost')
-    assert response.status_code == 200
-    assert b"Submit" in response.data
-    assert b"Post Research Opportunity" in response.data
+#     #test the "PostSmile" form 
+#     response = test_client.get('/createpost')
+#     assert response.status_code == 200
+#     assert b"Submit" in response.data
+#     assert b"Post Research Opportunity" in response.data
     
-    #test posting a smile story
-    tags1 = list( map(lambda t: t.id, Interest.query.all()[:12]))  # should only pass 'id's of the tags. See https://stackoverflow.com/questions/62157168/how-to-send-queryselectfield-form-data-to-a-flask-view-in-a-unittest
-    print("TESTING********************: ", tags1)
-    response = test_client.post('/createpost', 
-                          data=dict(title='My test post', description='This is my first test post.',qualifications="testing qual",start_date="test1", end_date = "test2", commitment = 1 ),
-                          follow_redirects = True)
-    #print(response.data)
-    assert response.status_code == 200
-    assert b"Create Post - Research Connect" in response.data
-    assert b"My test post" in response.data 
-    assert b"This is my first test post." in response.data 
+#     #test posting a smile story
+#     tags1 = list( map(lambda t: t.id, Interest.query.all()[:12]))  # should only pass 'id's of the tags. See https://stackoverflow.com/questions/62157168/how-to-send-queryselectfield-form-data-to-a-flask-view-in-a-unittest
+#     print("TESTING********************: ", tags1)
+#     response = test_client.post('/createpost', 
+#                           data=dict(title='My test post', description='This is my first test post.',qualifications="testing qual",start_date="test1", end_date = "test2", commitment = 1 ),
+#                           follow_redirects = True)
+#     #print(response.data)
+#     assert response.status_code == 200
+#     assert b"Create Post - Research Connect" in response.data
+#     assert b"My test post" in response.data 
+#     assert b"This is my first test post." in response.data 
 
-    c = db.session.query(Post).filter(Post.title =='My test post')
-    # assert c.first().get_interests().count() == 12 #should have 3 tags
-    assert c.count() >= 1 #There should be at least one post with body "Here is another post."
+#     c = db.session.query(Post).filter(Post.title =='My test post')
+#     # assert c.first().get_interests().count() == 12 #should have 3 tags
+#     assert c.count() >= 1 #There should be at least one post with body "Here is another post."
 
 
     # tags2 = list( map(lambda t: t.id, Tag.query.all()[1:3]))  # should only pass 'id's of the tags. See https://stackoverflow.com/questions/62157168/how-to-send-queryselectfield-form-data-to-a-flask-view-in-a-unittest
@@ -259,4 +246,70 @@ def test_login_logout(request,test_client,init_database):
     # assert b"Please log in to access this page." in response.data   
     
 
- """
+
+def test_student_register(test_client,init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/register' form is submitted (POST)
+    THEN check that the response is valid and the database is updated correctly
+    """
+    # Create a test client using the Flask application configured for testing
+    response = test_client.post('/student_registration', 
+                            data=dict(username = 'sejal@wsu.edu',phone_num = '1234', 
+                            firstname = 'Sejal',lastname = 'Welankar',
+                            wsu_id = '987654',major = 'Computer Science',
+                            gpa= '4.0', grad_date = 'May 2023',
+                            tech_electives = 'CS 121',languages = 'C, C++, Python', 
+                            prior_exp = 'TA for 121', password = '123', password2 = '123'),
+                            follow_redirects = True)
+
+    assert response.status_code == 200
+
+    s = db.session.query(Student).filter(Student.username =='sejal@wsu.edu')
+    assert s.first().wsu_id == '987654'
+    assert s.count() == 1
+    assert b"Click to Register" in response.data   
+    assert b"Please log in to access this page." in response.data
+
+
+def test_createpost(test_client,init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' form is submitted (POST) with wrong credentials
+    THEN check that the response is valid and login is refused 
+    """
+    response = test_client.post('/login', 
+                          data=dict(username='sakire@wsu.edu', password='123',remember_me=False),
+                          follow_redirects = True)
+    assert response.status_code == 200
+    assert b"Welcome to Research Connect!" in response.data
+
+    response = test_client.get('/createpost')
+    assert response.status_code == 200
+    assert b"Post Research Opportunity" in response.data
+    interests1 = list( map(lambda t: t.id, Interest.query.all()[:3]))
+    for i in interests1:
+        print(i)
+  # tags1 = list( map(lambda t: t.id, Tag.query.all()[:3]))  # should only pass 'id's of the tags. See https://stackoverflow.com/questions/62157168/how-to-send-queryselectfield-form-data-to-a-flask-view-in-a-unittest
+  
+    response = test_client.post('/createpost', 
+                          data=dict(title='My test post', description='This is my first test post.',qualifications="testing qual",
+                          start_date="test1", end_date = "test2", commitment = 1, interest = interests1 ),
+                          follow_redirects = True)
+    c = db.session.query(Post).filter(Post.title =='My test post')
+    assert c.first().get_interests().count() == 3 #should have 3 tags
+    assert c.count() == 1          
+    print(response.data)
+    assert response.status_code == 200
+
+    assert b"Welcome to Research Connect!" in response.data
+
+    assert b"My test post" in response.data 
+    assert b"This is my first test post." in response.data
+
+
+
+
+
+
+
